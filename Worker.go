23ed69f8d06ef61350a8b26358f83807e0560b66
Wirden <antoine.chevrot@gmail.com>
2017-06-03 16:43:35 +0200

    Worker in Go. Error handled : Empty String, malformed equations, only integers taken into account

---
+++ Worker.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var WRONG_SYNTHAX_ERROR = "Malformed notation"
+var VALID_MESSAGE = " is equals to "
+var ARGUMENT_NULL_ERROR = "An equation cannot be empty"
+
+func main() {
+
+	arg := os.Args[1]
+
+	if arg == "" {
+		log.Fatal(ARGUMENT_NULL_ERROR)
+	}
+
+	var stack []float64
+	for _, tok := range strings.Fields(arg) {
+		switch tok {
+		case "+":
+			if len(stack) < 2 {
+				error1()
+			} else {
+				stack[len(stack)-2] += stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			}
+		case "-":
+			if len(stack) < 2 {
+				error1()
+			} else {
+				stack[len(stack)-2] -= stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			}
+		case "*":
+			if len(stack) < 2 {
+				error1()
+			} else {
+				stack[len(stack)-2] *= stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			}
+		case "/":
+			if len(stack) < 2 {
+				error1()
+			} else {
+				stack[len(stack)-2] /= stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+			}
+		default:
+			if _, err := strconv.Atoi(tok); err == nil {
+				f, _ := strconv.ParseFloat(tok, 64)
+				stack = append(stack, f)
+			} else {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	fmt.Println(arg, VALID_MESSAGE, stack[0])
+}
+
+func error1() {
+	err := errors.New(WRONG_SYNTHAX_ERROR)
+	log.Fatal(err)
+}
